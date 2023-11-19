c24cdf87799e5d3c359492d09bb22e6f73dda9f7
DanielGomez <killergomez777@gmail.com>
2023-11-19 18:10:44 -0500

    consulta

---
+++ Models/Consulta.cs
@@ -0,0 +1,13 @@
+﻿namespace ParcialLenguajeInformatico.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public string NombreConsulta { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NombreMedico { get; set; }
+        public string Observacion { get; set; }
+        public int PacienteId { get; set; }
+        public Paciente? Paciente { get; set; }
+    }
+}
