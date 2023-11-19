303694cb1c2fb7a649633809e14e2225a857b513
DanielGomez <killergomez777@gmail.com>
2023-11-19 18:08:46 -0500

    paciente

---
+++ Models/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace ParcialLenguajeInformatico.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int edad { get; set; }
+        public string TipoDocumento { get; set; }
+        public int NroDocumento { get; set; }
+        public long Contacto { get; set; }
+        public ICollection<Consulta>? Consultas { get; set; }
+    }
+}
