1e4acf0a31b6413745cdd4d2a6cb4a406a3f8b59
DanielGomez <killergomez777@gmail.com>
2023-11-19 17:56:28 -0500

    Agregar archivos de proyecto.

---
+++ Data/ParcialContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialLenguajeInformatico.Models;
+
+namespace ParcialLenguajeInformatico.Data
+{
+    public class ParcialContext : DbContext
+    {
+        public ParcialContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Paciente> Paciente { get; set; }
+        public DbSet<Consulta> Consulta { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ParcialDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
